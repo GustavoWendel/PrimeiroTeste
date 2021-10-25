df096c909cbe83d1bec416b22d485c9418e62253
Gustavo Borges <gborges@sccalpha.local>
2021-10-25 09:40:46 -0300

    Adicionando Mockito

--- src/test/java/cursosdousa/MockitoTests.java
+++ src/test/java/cursosdousa/MockitoTests.java
@@ -2,22 +2,28 @@ package cursosdousa;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.ArrayList;
 import java.util.List;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class MockitoTests {
+
+    @Mock
+    List<String> lista;
 
     @Test
     public void primeiroTesteMockito(){
-        List<String> lista = Mockito.mock(ArrayList.class);
+        lista.size();
+        lista.add("");
 
-        Mockito.when(lista.size()).thenReturn(20);
+        InOrder inOrder = Mockito.inOrder(lista);
+        inOrder.verify(lista).size();
+        inOrder.verify(lista).add("");
 
-        int size = lista.size();
-
-        Assertions.assertThat(size).isEqualTo(20);
     }
 }
